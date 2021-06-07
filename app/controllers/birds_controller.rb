10fabb012b6439ac6613ae02084cc97db1f8657e
Doug Jones <DSJR1284@GMAIL.COM>
2021-06-06 20:35:44 -0400

    commit tree 7e09dff98bb33af3058a957d6bf07fbb9925fef5

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,53 @@
 class BirdsController < ApplicationController
+  # def index
+  #   birds = Bird.all
+  #   render json: birds
+  # end
+
+  # Only option Method 
+  # def index
+  #   birds = Bird.all
+  #   render json: birds, only: [:id, :name, :species]
+  # end
+
+  # Except option Method
+  # def index
+  #   birds = Bird.all
+  #   render json: birds, except: [:created_at, :updated_at]
+  # end
+
+  # Without the Rails Magic 
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds.to_json(except: [:created_at, :updated_at])
   end
+
+
+  # def show 
+  #   bird = Bird.find_by(id: params[:id])
+  #   render json: bird
+  # end 
+
+  # def show
+  #   bird = Bird.find_by(id: params[:id])
+  #   render json: {id: bird.id, name: bird.name, species: bird.species } 
+  # end
+
+  # Slice Method
+  # def show
+  #   bird = Bird.find_by(id: params[:id])
+  #   render json: bird.slice(:id, :name, :species)
+  # end
+
+  # Error Message 
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      render json: { id: bird.id, name: bird.name, species: bird.species }
+    else
+      render json: { message: 'Bird not found' }
+    end
+  end
+
+
 end
